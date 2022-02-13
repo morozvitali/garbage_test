ecbbf985fc8cae589719c18495eb8bc093dd081b
morozvitali <morozvitali99@gmail.com>
2022-02-13 23:08:48 +0200

    dog eat Tomatos

--- src/STACK/Stack/Stack.java
+++ src/STACK/Stack/Stack.java
@@ -14,56 +14,101 @@ package STACK.Stack;
 // Плюс, нам нужно переопределить toString(), чтобы он в понятном для нас виде выводил инфу о стеке.
 
 
+import java.util.Arrays;
+
 public class Stack {
     static int i = 0;
-    static Object[] objArray;
+    Object[] objArray = new Object[3];
+    boolean empty = true;
 
-    public Stack (int stackObjectNumber){
-        Object[] objArray = new Object[stackObjectNumber];
-    }
+//    public Stack (){
+//        Object[] objArray = ;
+//    }
 
-    static void push (Object obj) {
+    void push () {
+        Object newObject = new Object();
+        System.out.println("1создали newObject");
         if (i < objArray.length) {
-            objArray[i] = obj;
-            System.out.println("Object Added to Array, number in Array: " + i);
+            System.out.println("2проверили условие i < array.lenhgt");
+            objArray[i] = newObject;
+            System.out.println("Object Added to Array, number of Objects in Array: " + (i+1));
         } else {
             Object[] objNewArray = new Object[objArray.length + 1];
             for (int a = 0; a < objArray.length; a++) {
                 objNewArray[a] = objArray[a];
             }
-            objNewArray[i] = obj;
+            objNewArray[i] = newObject;
             objArray = objNewArray;
-            System.out.println("Array is Full, Object added, number in Array " + i);
+            System.out.println("Array is Full, Object added, number of Objects in Array " + (i+1));
         }
-        i++;
+
+        {   i++;
+            empty = false;
+        }
     }
 
-    static void pop () {
-        if (i > 0) {
+    void pop () {
+        if (!empty && i>1) {
             objArray[i - 1] = null;
             i--;
-            System.out.println("Object Deleted from Array, Array has elements: " + i);
-        } else if (i == 0) {
+            System.out.println("Object Deleted from Array, Array has elements: " + (i));
+        } else if (!empty) {
             objArray[0] = null;
-            System.out.println("Object deleted from Array, Array is empty");
+            System.out.println("Object deleted, Array is empty now");
+            empty = true;
         } else {
-            System.out.println("Array is empty, cannot delete Object");
+            empty = true;
+            System.out.println("Array empty");
         }
     }
 
-    static boolean isEmpty () {
-        if (i<0) {
+    boolean isEmpty () {
+        if (empty) {
             System.out.println("method isEmpty: Array empty");
             return true;}
     else
         System.out.println("method isEmpty: Array not empty");
         return false;
     }
-}
 
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "objArray=" + Arrays.toString(objArray) +
+                ", empty=" + empty +
+                '}';
+    }
 
-class Test {
     public static void main(String[] args) {
+        Stack st1 = new Stack();
+        System.out.println( st1.toString());
+        st1.isEmpty();
+        st1.push();
+        st1.push();
+        st1.push();
+        st1.push();
+        st1.push();
+        st1.isEmpty();
+        System.out.println( st1.toString());
+
+
+        st1.pop();
+        st1.pop();
+        st1.pop();
+        st1.pop();
+        st1.pop();
+        st1.pop();
+        st1.pop();
+        System.out.println( st1.toString());
+
+        st1.isEmpty();
+//        st1.isEmpty();
 
     }
+
+//    public void toString () {
+//
+//    }
+
 }
+
